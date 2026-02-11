1901356fdbae6e9f4b1b3c6e88bc141cf0cb80bd
paulmendoza123 <mendozapaul477@gmail.com>
2026-02-11 18:20:03 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,10 +1,55 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+# -------------------------------
+# Task A: CollegeAdmin
+# -------------------------------
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+# -------------------------------
+# Task B: ProgramAdmin
+# -------------------------------
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+# -------------------------------
+# Task C: OrganizationAdmin
+# -------------------------------
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+# -------------------------------
+# StudentAdmin
+# -------------------------------
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+    list_filter = ("program",)
+
+# -------------------------------
+# OrgMemberAdmin
+# -------------------------------
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname",)
+    list_filter = ("organization",)
+
+    # Custom method to show the program of the student
+    def get_member_program(self, obj):
+        try:
+            return obj.student.program
+        except Student.DoesNotExist:
+            return None
+    get_member_program.short_description = "Program"
