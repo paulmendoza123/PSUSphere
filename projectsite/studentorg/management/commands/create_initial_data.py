bd09ea096f2004bc91f1ed34af96c474a31545a6
paulmendoza123 <mendozapaul477@gmail.com>
2026-02-20 22:36:13 +0800

    updated with bootstrap

--- projectsite/studentorg/management/commands/create_initial_data.py
+++ projectsite/studentorg/management/commands/create_initial_data.py
@@ -2,50 +2,35 @@ from django.core.management.base import BaseCommand
 from faker import Faker
 from studentorg.models import College, Program, Organization, Student, OrgMember
 
+
 class Command(BaseCommand):
     help = 'Create initial data for the application'
 
     def handle(self, *args, **kwargs):
-        self.create_colleges(8)
-        self.create_programs(10)
         self.create_organization(10)
         self.create_students(50)
         self.create_membership(10)
 
-    def create_colleges(self, count):
-        fake = Faker()
-        for _ in range(count):
-            College.objects.create(
-                college_name=f"College of {fake.word().title()}"
-            )
-        self.stdout.write(self.style.SUCCESS(
-            'Initial data for colleges created successfully.'))
-
-    def create_programs(self, count):
-        fake = Faker()
-        for _ in range(count):
-            Program.objects.create(
-                prog_name=f"Bachelor of Science in {fake.word().title()}",
-                college=College.objects.order_by('?').first()
-            )
-        self.stdout.write(self.style.SUCCESS(
-            'Initial data for programs created successfully.'))
-
     def create_organization(self, count):
         fake = Faker()
+
         for _ in range(count):
-            words = [fake.word() for _ in range(2)]
+            words = [fake.word() for _ in range(2)]  # two words
             organization_name = ' '.join(words)
+
             Organization.objects.create(
                 name=organization_name.title(),
                 college=College.objects.order_by('?').first(),
                 description=fake.sentence()
             )
+
         self.stdout.write(self.style.SUCCESS(
-            'Initial data for organization created successfully.'))
+            'Initial data for organization created successfully.'
+        ))
 
     def create_students(self, count):
         fake = Faker('en_PH')
+
         for _ in range(count):
             Student.objects.create(
                 student_id=f"{fake.random_int(2020,2025)}-{fake.random_int(1,8)}-{fake.random_number(digits=4)}",
@@ -54,17 +39,21 @@ class Command(BaseCommand):
                 middlename=fake.last_name(),
                 program=Program.objects.order_by('?').first()
             )
+
         self.stdout.write(self.style.SUCCESS(
-            'Initial data for students created successfully.'))
+            'Initial data for students created successfully.'
+        ))
 
     def create_membership(self, count):
         fake = Faker()
+
         for _ in range(count):
             OrgMember.objects.create(
                 student=Student.objects.order_by('?').first(),
                 organization=Organization.objects.order_by('?').first(),
-                date_joined=fake.date_between(
-                    start_date="-2y", end_date="today")
+                date_joined=fake.date_between(start_date="-2y", end_date="today")
             )
+
         self.stdout.write(self.style.SUCCESS(
-            'Initial data for student organization created successfully.'))
+            'Initial data for student organization created successfully.'
+        ))
