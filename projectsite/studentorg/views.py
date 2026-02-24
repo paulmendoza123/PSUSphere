8163ac8911952fc21ef8c1ae302c70368cdaeee4
Kevin-Docog <docogkevin72@gmail.com>
2026-02-24 22:56:28 +0800

    add organization member feature

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, OrgMember
-from studentorg.forms import OrganizationForm
+from studentorg.forms import OrganizationForm, OrgMemberForm
 from django.urls import reverse_lazy
 
 
@@ -41,4 +41,10 @@ class OrgMemberList(ListView):
     context_object_name = 'orgmember'
     template_name = 'orgmember_list.html'
     paginate_by = 5
+
+class OrgMemberCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_form.html'
+    success_url = reverse_lazy('orgmember-list')
 
